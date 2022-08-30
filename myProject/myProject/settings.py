c34c1c9c5b650fd11b9e50247a2d7529ce8a974a
bruno <bruno.mcreal@gmail.com>
2022-08-30 17:20:08 -0300

    layout Bootstrap

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [STATIC_DIR, "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
