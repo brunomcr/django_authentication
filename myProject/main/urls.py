f36a950e6357d502bba11d5d4bf020c191bbbcd9
bruno <bruno.mcreal@gmail.com>
2022-08-30 09:03:37 -0300

    Run Black (format PEP8)

--- myProject/main/urls.py
+++ myProject/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'main'
+app_name = "main"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("authenticated_view/", views.authenticated_view, name="authenticated_view"),
